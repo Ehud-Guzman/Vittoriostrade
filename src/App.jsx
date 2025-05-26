e54e5e458ee12fba18ffb84f35cca91ec3f2c0dd
Guzman <nyamuehud@gmail.com>
2025-05-26 10:00:30 +0300

    Add ScrollToTop component for route-based scroll reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -9,6 +10,7 @@ import Contact from './pages/Contact';
 function App() {
   return (
     <BrowserRouter>
+    <ScrollToTop />
       <div className="min-h-screen flex flex-col">
         <Header />
         <main className="flex-grow">
